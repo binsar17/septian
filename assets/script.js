ac16f575e8ff640bd16b3674e1cfac06c6d1882a
Vsept17 <verzchseptian@gmail.com>
2020-05-16 06:59:28 +0700

    done

---
+++ assets/script.js
@@ -0,0 +1,71 @@
+document.getElementById('foto1').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto1').requestFullscreen();
+  }
+});
+
+document.getElementById('foto2').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto2').requestFullscreen();
+  }
+});
+
+document.getElementById('foto3').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto3').requestFullscreen();
+  }
+});
+
+document.getElementById('foto4').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto4').requestFullscreen();
+  }
+});
+
+document.getElementById('foto5').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto5').requestFullscreen();
+  }
+});
+
+document.getElementById('foto6').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto6').requestFullscreen();
+  }
+});
+
+document.getElementById('foto7').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto7').requestFullscreen();
+  }
+});
+
+document.getElementById('foto8').addEventListener('click', (event) => {
+  if (document.fullscreenElement) {
+    // exitFullscreen is only available on the Document object.
+    document.exitFullscreen();
+  } else {
+    document.getElementById('foto8').requestFullscreen();
+  }
+});
